28323867057ac683623273d5e02d6c53ea6c2b6c
Nikhil V M <nikhil1311.nk@gmail.com>
2022-04-15 19:33:57 -0400

    Random test submission

--- agent.py
+++ agent.py
@@ -5,17 +5,21 @@ class Agent():
      You are allowed to add any method you
      want to this class.
   '''
+  eps = 0.3
 
   def __init__(self, env_specs):
     self.env_specs = env_specs
+    self.reward_list = [(env_specs['action_space'].sample(), 0)]
 
-  def load_weights(self, root_path):
-    # Add root_path in front of the path of the saved network parameters
-    # For example if you have weights.pth in the GROUP_MJ1, do `root_path+"weights.pth"` while loading the parameters
+  def load_weights(self):
     pass
 
   def act(self, curr_obs, mode='eval'):
-    return self.env_specs['action_space'].sample()
+    if random.uniform(0, 1) > self.eps:
+      # return sorted(self.reward_list, key=lambda item: item[1], reverse=True)[0][0] try this a[np.argmax(np.array(a), axis=0)[1]][0]
+      return self.reward_list[np.argmax(self.reward_list, axis=0)[1]][0]
+    else:
+      return self.env_specs['action_space'].sample()
 
   def update(self, curr_obs, action, reward, next_obs, done, timestep):
-    pass
+    self.reward_list.append((action, reward))
