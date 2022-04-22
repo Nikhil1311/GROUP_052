718822c7e3291bc0ecfe18f30e24c5c5301a7ee3
Hattie Zhou <hattie@Ashley.localdomain>
2022-04-22 13:58:39 -0400

    update

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=15:00:00
-#SBATCH -c 6
-#SBATCH --mem=32G
+#SBATCH -c 2
+#SBATCH --mem=8G
 #SBATCH --gres=gpu:1
 
 source ~/rl_mujoco/bin/activate
@@ -12,4 +12,4 @@ module load python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.7.0
 wandb login
 cd ~/GROUP_052
 
-python train_agent.py --group_name reset_every_100000_inter9 --step_per_inter 9 --reset_every 100000 --use_wandb --wandb_dir $SLURM_TMPDIR
+python train_agent.py --group_name hidden512_reset_every_500000_bs256_seedstep10000_inter5_lr1e-3_seed0 --step_per_inter 5 --batch_size 256 --reset_every 500000 --num_seed_steps 10000 --use_wandb --hidden_dim 512 --seed 0 --wandb_dir $SLURM_TMPDIR
